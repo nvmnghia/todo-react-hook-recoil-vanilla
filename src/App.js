1e5b2550bc60686794512f44ce710754387fb5da
nvmnghia <nvmnghia@gmail.com>
2022-02-22 13:13:06 +0700

    feat: state setup & modification in `<App>`

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
-import { BrowserRouter } from "react-router-dom";
+import { useState, useEffect } from 'react';
+
+import { BrowserRouter } from 'react-router-dom';
+
+import { todoFromContent } from './Todo';
+import { loadFromLocalStorage, saveToLocalStorage } from './local_storage';
 
 function App() {
+  // State setup
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    setTodos(loadFromLocalStorage()); // Load initial
+  }, []);
+  useEffect(() => {
+    saveToLocalStorage(todos); // Save if changed
+  }, [todos]);
+
+  // Edit state
+  const addTodo = (content) => {
+    // Todo constructor has side effect, thus can't be used inside updater function
+    const newTodo = todoFromContent(content);
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
+  };
+  const removeTodo = (id) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+  const editTodo = (id, content) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { id, content, date: new Date() } : todo
+      )
+    );
+  };
+
   return (
   <BrowserRouter>
    <div>nothing</div>
