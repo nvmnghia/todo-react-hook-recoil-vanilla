2e920a71ca119a4cd42e8a1550f66131e36ae186
nvmnghia <nvmnghia@gmail.com>
2022-02-22 13:54:24 +0700

    feat: remove `index.css`

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/js/bootstrap.bundle';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
