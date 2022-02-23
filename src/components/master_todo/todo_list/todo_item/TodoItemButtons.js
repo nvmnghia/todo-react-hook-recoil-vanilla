6c5b6ad1d905a2fb80b475a5f3190eb22281e4ff
nvmnghia <nvmnghia@gmail.com>
2022-02-23 11:08:49 +0700

    feat: add `title` for buttons

--- src/components/master_todo/todo_list/todo_item/TodoItemButtons.js
+++ src/components/master_todo/todo_list/todo_item/TodoItemButtons.js
@@ -9,25 +9,25 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 
 const EditButton = ({ onStartEdit }) => (
-  <button className='btn btn-outline-primary' onClick={onStartEdit}>
+  <button className='btn btn-outline-primary' onClick={onStartEdit} title='Edit'>
     <FontAwesomeIcon icon={faPen} />
   </button>
 );
 
 const SaveButton = ({ onSave }) => (
-  <button className='btn btn-outline-primary' onClick={onSave}>
+  <button className='btn btn-outline-primary' onClick={onSave} title='Save'>
     <FontAwesomeIcon icon={faCheck} />
   </button>
 );
 
 const UndoButton = ({ onClick }) => (
-  <button className='btn btn-outline-danger' onClick={onClick}>
+  <button className='btn btn-outline-danger' onClick={onClick} title='Undo'>
     <FontAwesomeIcon icon={faRotateLeft} />
   </button>
 );
 
 const RemoveButton = ({ onRemove }) => (
-  <button className='btn btn-outline-danger' onClick={onRemove}>
+  <button className='btn btn-outline-danger' onClick={onRemove} title='Remove'>
     <FontAwesomeIcon icon={faXmark} />
   </button>
 );
